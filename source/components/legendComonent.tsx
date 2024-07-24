1af24775835248e09c123a6a8594b1481ab6545f
EagleoutIce <florian.sihler@uni-ulm.de>
2024-07-24 11:08:40 +0200

    refactor: minor css updates :sparkles:

--- source/components/legendComonent.tsx
+++ source/components/legendComonent.tsx
@@ -6,18 +6,18 @@ interface LegendComponentProps {
 
 export function slideInLegend() {
     const element: HTMLDivElement = document.getElementById('slide-in-legend') as HTMLDivElement ?? new HTMLDivElement()
-    element.className = 'slide-in-legend-visible'
+    element.classList.toggle('visible');
 }
 
 export function slideOutLegend(){
     const element: HTMLDivElement = document.getElementById('slide-in-legend') as HTMLDivElement ?? new HTMLDivElement()
-    element.className = 'slide-in-legend-invisible'
+    element.classList.toggle('visible');
 }
 
 export const LegendComponent: React.FC<LegendComponentProps> = (props) => {
 
     return (
-        <div className = 'slide-in-legend-invisible' id = 'slide-in-legend' >
+        <div className = 'slide-in-legend' id = 'slide-in-legend' >
             <div id = 'legend-close-button-div'>
                 <button className = {'button-close-legend'} onClick = {slideOutLegend} >X</button>
             </div>
